d95ff72380ab7cba2a5e9841d63930397e769761
Lorenzo Julian Suarez Almeyra <lsuareza2005@gmail.com>
2025-06-29 02:20:36 -0300

    primer servidor + guarda datos en json

--- first-server/public/main.js
+++ first-server/public/main.js
@@ -1,24 +1,103 @@
+//BOTON DE VOLVER A INICIO EN CASO DE QUE LA PAG NO EXISTA
 const backHome = document.getElementById("returnHome");
 if (backHome) {
     backHome.addEventListener("click",() => {
-        window.location.href = "index.html";
+        window.location.href = "/";
     })
 }
+//BOTON QUE REDIRECCIONA AL USUARIO A UNA PAG QUE NO EXISTE
 const forgotPass = document.getElementById("forgotPassword");
 if (forgotPass){
     forgotPass.addEventListener("click", () => {
-        window.location.href="error404.html";
+        window.location.href="/forgot-password";
     })
 }
+//BOTONES QUE REDIRECCIONAN AL USUARIO A LA PAG DE LOGIN O SIGNUP
 const login = document.getElementById("logIn");
 if (login) {
     login.addEventListener("click", () => {
-        window.location.href="login.html";
+        window.location.href="/login";
     })
 }
 const signUp = document.getElementById("signUp");
 if (signUp){
     signUp.addEventListener("click", () => {
-        window.location.href="signup.html";
+        window.location.href="/signup";
     })
+}
+//BOTON PARA SUBIR LA INFORMACION DEL REGISTRO
+const submit = document.getElementById("submit");
+if (submit){
+    submit.addEventListener("click", (e) => {
+        e.preventDefault();
+        const username = document.getElementById("username").value;
+        const email = document.getElementById("email").value;
+        const firstPass = document.getElementById("firstPass").value;
+        const secPass = document.getElementById("secPass").value;
+        let password;
+        if (firstPass === secPass){
+            password = firstPass
+        }
+        else{
+            alert('Las contraseñas no coinciden')
+            return
+        }
+        if (username && email && password) {
+            fetch('http://localhost:3000/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({username, email, password })
+            })
+            .then(res => {
+                if (!res.ok)
+                    throw new Error ('Something went wrong')
+                return res.json()
+            })
+            .then(data => {
+                console.log(data.error || data.msg)
+            })
+            .catch(err => {
+                console.error(err)
+            })
+        } else {
+            alert("Por favor, completa todos los campos.");
+        }
+    })
+}
+//BOTON PARA SUBIR LA INFORMACION DEL INICIO DE SESION
+const submitLogin = document.getElementById("submitLogin");
+if (submitLogin) {
+    submitLogin.addEventListener("click", (e) => {
+        e.preventDefault();
+        const username = document.getElementById("username").value;
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        if (username && email && password) {
+            fetch('http://localhost:3000/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username, email, password })
+            })
+            .then(res => res.json().then(data => {
+                if (!res.ok) {
+                    throw new Error(data.error || 'Something went wrong');
+                }
+                return data;
+            }))
+            .then(data => {
+                console.log(data.msg || data.error)
+                if (data.msg)
+                    alert('Login realizado con exito')
+            })
+            .catch(err => {
+                console.error(err)
+            })
+        } else {
+            alert('Falta ingresar datos')
+        }
+    });
 }
--- first-server/server.js
+++ first-server/server.js
@@ -1,18 +1,68 @@
 const express = require('express')
-const fs = require('fs')
+const path = require('path')
+const fs = require('fs');
 
 const app = express();
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
 const port = 3000;
+const usersDir = path.join(__dirname,'users.json')
 
 app.get('/', (req,res) => {
-    res.sendFile('./public/index.html',{root: __dirname}) //root indica que lee el archivo desde este directorio
+    res.sendFile(path.join(__dirname,'public','htmls','index.html'))
 })
 app.get('/login', (req,res) => {
-    res.sendFile('./public/login.html',{root: __dirname})
+    res.sendFile(path.join(__dirname,'public','htmls','login.html'))
+})
+app.get('/signup', (req,res) => {
+    res.sendFile(path.join(__dirname,'public','htmls','signup.html'))
+})
+app.post('/signup',(req,res) => {
+    const newUser = req.body
+    console.log('Nuevo usuario: ',newUser)
+    if (!newUser.username || !newUser.email || !newUser.password)
+        return res.status(400).json({ error: 'the fields are missing.' });
+    if (exist(newUser)){
+        alert('Este usuario ya existe')
+        return res.status(400).json({error: 'user already exists'})
+    }
+    const users = getUsers()
+    users.push(newUser)
+    addNewUser(users)
+    res.json({msg: 'user added successfully'})
+})
+app.post('/login',(req,res) => {
+    const data = req.body
+    console.log('Nuevo inicio de sesion: ',data)
+    if(exist(data)){
+        console.log('Login realizado con exito')
+        res.json({msg: 'successful login'})
+    }
+    else {
+        res.status(401).json({error: 'unsuccessful login'})
+    }
 })
 app.use((req,res) => {
-    res.status(404).sendFile('./public/error404.html',{root: __dirname})
+    res.status(404).sendFile(path.join(__dirname,'public','htmls','error404.html'))
     console.error('Page not found')
 })
+function addNewUser (users) {
+    fs.writeFileSync(usersDir,JSON.stringify(users))
+}
+function exist (user) {
+    const users = getUsers()
+    const aux = users.find(u => u.username === user.username && u.email === user.email && u.password === user.password)
+    if (aux)
+        return true
+    return false
+}
+function getUsers() {
+    try{
+        return JSON.parse(fs.readFileSync(usersDir).toString('utf8'))
+    }
+    catch{
+        return []
+    }
+}
 app.listen(port)
 console.log(`Server on port ${port}`)
